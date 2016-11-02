54951b06bd325287ccc837fe03ee374d0072e3d3
Kevin Comedia <kevcomedia@gmail.com>
2016-11-02 15:10:38 +0000

    Initial script

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
+app.get('/:time', function(req, res) {
+    'use strict';
+    
+    let input = req.params.time;
+    if (/^(\+|-)?\d+/.test(input)) {
+        input = Number.parseInt(input, 10);
+    }
+
+    const date = new Date(input);
+    const output = {
+        unix: null,
+        natural: null
+    };
+
+    if (!Number.isNaN(date.getTime())) {
+        output.unix = date.getTime();
+        output.natural = date.toLocaleString('en-US', {
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric'
+        });
+    }
+
+    res.send(output);
 });
 
 app.listen(8080, function() {
-   console.log('Live on port 8080!') ;
+    console.log('Live on port 8080!');
 });
