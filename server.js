4434c6cb5e7fd1001053cda6ecdd0c3b42a89ec0
Kevin Comedia <kevcomedia@gmail.com>
2016-11-02 16:03:49 +0000

    Reduce leading space width

--- server.js
+++ server.js
@@ -2,27 +2,27 @@ const express = require('express');
 const app = express();
 
 app.get('/:time', function(req, res) {
-    'use strict';
+  'use strict';
 
-    const date = new Date(/^(\+|-)?\d+$/.test(req.params.time) ? req.params.time * 1000 : req.params.time);
-    const output = {
-        unix: null,
-        natural: null
-    };
+  const date = new Date(/^(\+|-)?\d+$/.test(req.params.time) ? req.params.time * 1000 : req.params.time);
+  const output = {
+    unix: null,
+    natural: null
+  };
 
-    if (!Number.isNaN(date.getTime())) {
-        output.unix = Math.floor(date.getTime() / 1000);
-        output.natural = date.toLocaleString('en-US', {
-            month: 'long',
-            day: 'numeric',
-            year: 'numeric'
-        });
-    }
+  if (!Number.isNaN(date.getTime())) {
+    output.unix = Math.floor(date.getTime() / 1000);
+    output.natural = date.toLocaleString('en-US', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric'
+    });
+  }
 
-    res.set({ 'Access-Control-Allow-Origin': '*' });
-    res.send(output);
+  res.set({ 'Access-Control-Allow-Origin': '*' });
+  res.send(output);
 });
 
 app.listen(8080, function() {
-    console.log('Live on port 8080!');
+  console.log('Live on port 8080!');
 });
